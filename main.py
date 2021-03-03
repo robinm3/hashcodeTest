305f182a3feb883da5b56515ca7c99fc6d932284
marobin <marc-antoine.robin.1@ulaval.com>
2021-03-03 15:49:44 -0500

    added output formating

--- main.py
+++ main.py
@@ -4,15 +4,40 @@ import fileFormatting
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def test():
+    command1 = {
+        "drone_id": 1234,
+        "tag": "L",
+        "warehouse_id": 123,
+        "product_type_id": 456,
+        "number_of_items": 4
+    }
+    command2 = {
+        "drone_id": 456,
+        "tag": "U",
+        "warehouse_id": 456,
+        "product_type_id": 456,
+        "number_of_items": 2
+    }
+    command3 = {
+        "drone_id": 456,
+        "tag": "D",
+        "customer_id": 456,
+        "product_type_id": 456,
+        "number_of_items": 2
+    }
+    command4 = {
+        "drone_id": 789,
+        "tag": "W",
+        "number_of_turns": 3
+    }
+    fileFormatting.read_file()
+    fileFormatting.write_file(4, [command1, command2, command3, command4])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    fileFormatting.read_file()
+    test()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
