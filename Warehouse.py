f939ab8275113b145a7f8b7526141e885609dfb2
Alexandre <alexandre.aubut.1@ulaval.ca>
2021-03-03 17:19:23 -0500

    avec id

--- Warehouse.py
+++ Warehouse.py
@@ -1,13 +1,16 @@
 import fileFormatting
 
 class Warehouse:
-    def __init__(self, position, items):
+    def __init__(self, position, items, id):
         self.position = position
         self.items = items
         self.ordre_commande = []
+        self.id = id
     
     def mes_commandes(self):pass
 
 liste_warehouse = []
+id = 0
 for i in fileFormatting.read_file()["warehouses"]:
-    liste_warehouse.append(Warehouse((i["row"], i["column"]), i["items"]))
+    liste_warehouse.append(Warehouse((i["row"], i["column"]), i["items"], id))
+    id += 1
